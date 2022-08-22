f1546e6fab28c77a305f0e6df37a8f505cbad4e1
radwan susan <radwansusan90@gmail.com>
2022-08-22 11:57:25 +0300

    Big Changes

---
+++ JS/personalInformation.js
@@ -0,0 +1,13 @@
+let title22 = document.querySelector(".toggle-menu");
+let list = document.getElementsByClassName("list");
+let list1 = document.getElementsByClassName("list1");
+let arf = document.getElementsByClassName("hvr-pulse-shrink");
+title22.onclick = () => {
+	list[0].classList.toggle("show1");
+	list1[0].classList.toggle("show2");
+	arf[0].classList.toggle("show3");
+	for (let i = 1; i < 4; i++) {
+		list1[i].classList.toggle("show2");
+		arf[i].classList.toggle("show3");
+	}
+};
